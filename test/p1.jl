5091f60f1331fa44b5c25b13d7f4d6903719010a
Dmitry <deemonster@gmail.com>
2014-06-04 16:07:18 -0700

    Split impl, move library methods to src.

--- test/p1.jl
+++ test/p1.jl
@@ -1,104 +1,20 @@
 using Base.Test
 
-type CommandArgs
-  sym::Symbol
-  args
-  CommandArgs() = new()
-end
-
-abstract Updater{T}
-
-immutable StructUpdater{T} <: Updater{T}
-  sym::Symbol
-end
-
-function update!(o::CommandArgs, args::Array{String,1})
-  if length(args) < 1
-    throw(ParseException("Expected command, args=$(args)"))
-  end
-  cmd, args = args[1], args[2:end]
-  if cmd == "move"
-    o.sym = :move
-    o.args = MoveArgs()
-    update!(o.args, args)
-  elseif cmd == "ls"
-    o.sym = :ls
-    o.args = LsArgs()
-    update!(o.args, args)
-  else
-    throw(ParseError("Unexpected command [$cmd]"))
-  end
-end
-
-function update!{R}(o::R, args::Array{String,1})
-  unparsed = Array{String,1}
-  i_arg = 1
-  consumed = 0
-  while i_arg <= length(args)
-    arg = args[i_arg]
-    if beginswith(arg, "-")
-      args_split = split(arg, "=", 2)
-      if length(args_split) == 1
-        p_arg = parser(o, arg)
-        if !isa(p_arg, Nothing)
-          v = valency(p_arg)
-          consumed = 1+v
-          update_args = args[i_arg:i_arg+v]
-        end
-      else
-        p_arg = parser(o, args_split[1])
-        if !isa(p_arg, Nothing)
-          v = valency(p_arg)
-          if v != 1
-            throw(ParserError("--option=value should be used only with valency=1, p=[$p_arg]"))
-          end
-          consumed = 1
-          update_args = args_split
-        end
-      end
-      update!(o, p_arg, convert(Array{String,1}, update_args))
-    end
-    if 0 < consumed
-      i_arg += consumed
-    else
-      push!(unparsed, args[i_arg])
-      i_arg += 1
-    end
-  end
-  return unparsed
-end
-
-valency(::StructUpdater{String}) = 1
-valency(::StructUpdater{Int}) = 1
-valency(::StructUpdater{Bool}) = 0
-
-function update!{R}(o::R, p::StructUpdater{String}, args::Array{String,1})
-  @assert 2 == length(args)
-  setfield!(o, p.sym, args[2])
-end
-
-function update!{R}(o::R, p::StructUpdater{Int}, args::Array{String,1})
-  @assert 2 == length(args)
-  setfield!(o, p.sym, int(args[2]))
-end
-
-function update!{R}(o::R, p::StructUpdater{Bool}, args::Array{String,1})
-  @assert 1 == length(args)
-  setfield!(o, p.sym, true)
-end
+require("src/args.jl")
+import args: CommandArgs, StructUpdater, update!, parser
 
 # ls --dir=/path/ls
 ls(dir="/path/ls") = "ls dir=$dir"
 
 # { generated: ls specific
-type LsArgs
+type _ls_args
   dir::String
-  LsArgs() = new("")
+  _ls_args() = new("")
 end
 
-ls(o::LsArgs) = ls(o.dir)
+ls(o::_ls_args) = ls(o.dir)
 
-function parser(p::LsArgs, arg::String)
+function parser(p::_ls_args, arg::String)
   if arg == "--dir" || arg == "-d"
     StructUpdater{String}(:dir)
   else
@@ -118,16 +34,16 @@ function move(from::String, to::String, recursive::Bool=false)
 end
 
 # { generated: Move specific
-type MoveArgs
+type _move_args
   from::String
   to::String
   recursive::Bool
-  MoveArgs() = new("", "", false)
+  _move_args() = new("", "", false)
 end
 
-move(o::MoveArgs) = move(o.from, o.to, o.recursive)
+move(o::_move_args) = move(o.from, o.to, o.recursive)
 
-function parser(p::MoveArgs, arg::String)
+function parser(p::_move_args, arg::String)
   if arg == "--from"
     StructUpdater{String}(:from)
   elseif arg == "--to"
@@ -140,9 +56,32 @@ function parser(p::MoveArgs, arg::String)
 end
 # }
 
+# { generated: main
+
+function update!(o::CommandArgs, args::Array{String,1})
+  if length(args) < 1
+    throw(ParseException("Expected command, args=$(args)"))
+  end
+  cmd, args = args[1], args[2:end]
+  if cmd == "move"
+    o.sym = :move
+    o.args = _move_args()
+    update!(o.args, args)
+  elseif cmd == "ls"
+    o.sym = :ls
+    o.args = _ls_args()
+    update!(o.args, args)
+  else
+    throw(ParseError("Unexpected command [$cmd]"))
+  end
+end
+
+
+# }
+
 function parse_args()
   args = String["--from", "/path/from", "--to", "/path/to", "-r"]
-  o = MoveArgs()
+  o = _move_args()
   args1 = update!(o, args)
 
   @test "/path/from" == o.from
@@ -152,7 +91,7 @@ end
 
 function parse_args1()
   args = String["--from=/path/from", "--to", "/path/to", "-r"]
-  o = MoveArgs()
+  o = _move_args()
   args1 = update!(o, args)
 
   @test "/path/from" == o.from
